f6e20c92e69814786b6309d1698b32d60fdc6365
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-09-23 12:12:12 -0400

    Add working example of an Interface
    Fix the broken Interface example.  This now shows, approximately, what
    it looks like for an arbitrary Chisel Module to "conform" to a
    port-level interface.  This is still very rough, but gets the idea
    across.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- src/main/scala/separable/Interface.scala
+++ src/main/scala/separable/Interface.scala
@@ -1,21 +1,45 @@
 package separable
 
-import chisel3.{BlackBox => _, _}
-import chisel3.experimental.FlatIO
+import chisel3.{BlackBox => _, Module => _, _}
+import chisel3.experimental.{ChiselAnnotation, FlatIO, RunFirrtlTransform}
 import chisel3.util.experimental.InlineInstance
+import firrtl.annotations.Annotation
+import firrtl.Transform
+import firrtl.passes.{InlineAnnotation, InlineInstances}
+import firrtl.transforms.NoDedupAnnotation
 
 trait Interface[A <: Record, B <: RawModule] {
 
   protected def genIO: A
 
-  protected def genModule: B with InlineInstance
+  protected def genModule: B
 
-  protected def convert(b: B): A
+  protected def connect(lhs: A, rhs: B): Unit
 
   class Module extends RawModule {
-    val io = FlatIO(Input(genIO))
+    val io = FlatIO(genIO)
 
-    io := convert(Module(genModule))
+    val internal = chisel3.Module(genModule)
+
+    val w = Wire(io.cloneType)
+    connect(w, internal)
+
+    io <> w
+
+    // TODO: This needs to not be explicitly specified.
+    override def desiredName = "BarWrapper"
+
+    Seq(
+      new ChiselAnnotation with RunFirrtlTransform {
+        def toFirrtl:       Annotation = InlineAnnotation(internal.toNamed)
+        def transformClass: Class[_ <: Transform] = classOf[InlineInstances]
+      },
+      new ChiselAnnotation {
+        def toFirrtl: Annotation = NoDedupAnnotation(internal.toNamed)
+      }
+    )
+      .map(chisel3.experimental.annotate(_))
+
   }
 
 }
@@ -26,60 +50,65 @@ object InterfacesMain extends App {
     * is set by specification.
     */
   class BarBundle extends Bundle {
-    val a = Bool()
-    val b = Flipped(Bool())
+    val a = Input(Bool())
+    val b = Output(Bool())
   }
 
-  /** This is the "DUT" that will be compiled in one compilation unit and reused
-    * multiple times. The designer is free to do whatever they want with this,
-    * internally or at the boundary. Note: this has ports which do not match the
-    * names of the ports on the agreed upon interface.
-    */
-  class Bar extends RawModule {
-    val x = IO(Input(Bool()))
-    val y = IO(Output(Bool()))
+  object CompilationUnit1 {
 
-    y := ~x
-  }
+    /** This is the "DUT" that will be compiled in one compilation unit and
+      * reused multiple times. The designer is free to do whatever they want
+      * with this, internally or at the boundary. Note: this has ports which do
+      * not match the names of the ports on the agreed upon interface.
+      */
+    class Bar extends RawModule {
+      val x = IO(Input(Bool()))
+      val y = IO(Output(Bool()))
+      y := ~x
+    }
 
-  /** The owner of the "DUT" (Bar) needs to write this. This defines how to hook
-    * up the "DUT" to the specification-set interface.
-    */
-  val BarInterface = new Interface[BarBundle, Bar] {
-    protected override def genIO = new BarBundle
-    protected override def genModule = new Bar with InlineInstance
-    protected override def convert(bar: Bar): BarBundle = {
-      val w = Wire(genIO)
-      // bar.x := w.a
-      // w.b := bar.ya
-      w
+    /** The owner of the "DUT" (Bar) needs to write this. This defines how to
+      * hook up the "DUT" to the specification-set interface.
+      */
+    val BarInterface = new Interface[BarBundle, Bar] {
+      protected override def genIO = new BarBundle
+      protected override def genModule = new Bar
+      protected override def connect(lhs: BarBundle, bar: Bar) = {
+        bar.x := lhs.a
+        lhs.b := bar.y
+      }
     }
   }
 
-  /** This is the boundary of a separable compilation unit. This is just a
-    * BlackBox that has the interface as its sole io. This is written by the
-    * spec author. (This can be simplified to be auto-generated from BarBundle.)
-    */
-  class BarBlackBox extends chisel3.BlackBox {
-    val io = IO(Input(new BarBundle))
+  object CompilationUnit2 {
 
-    override def desiredName = "Bar"
-  }
+    /** This is the boundary of a separable compilation unit. This is just a
+      * BlackBox that has the interface as its sole io. This is written by the
+      * spec author. (This can be simplified to be auto-generated from
+      * BarBundle.)
+      */
+    class BarBlackBox extends chisel3.BlackBox {
+      val io = IO(new BarBundle)
 
-  /** This is a module above the "DUT" (Bar). This stamps out the "DUT" twice,
-    * but using the blackbox version of it that conforms to the
-    * specification-set port list.
-    */
-  class Foo extends RawModule {
-    val a = IO(Input(Bool()))
-    val b = IO(Output(Bool()))
+      // TODO: This needs to not be explicitly specified.
+      override def desiredName = "BarWrapper"
+    }
 
-    val bar1 = Module(new BarBlackBox)
-    val bar2 = Module(new BarBlackBox)
+    /** This is a module above the "DUT" (Bar). This stamps out the "DUT" twice,
+      * but using the blackbox version of it that conforms to the
+      * specification-set port list.
+      */
+    class Foo extends RawModule {
+      val a = IO(Input(Bool()))
+      val b = IO(Output(Bool()))
 
-    bar1.io.a := a
-    bar2.io.a := bar1.io.b
-    b := bar2.io.b
+      val bar1 = chisel3.Module(new BarBlackBox)
+      val bar2 = chisel3.Module(new BarBlackBox)
+
+      bar1.io.a := a
+      bar2.io.a := bar1.io.b
+      b := bar2.io.b
+    }
   }
 
   /** Now we compile the design into the "build/Interfaces" directory. Both
@@ -88,7 +117,11 @@ object InterfacesMain extends App {
     * everything works.
     */
   private val dir = new java.io.File("build/Interfaces")
-  Drivers.compile(dir, () => new Foo, () => new (BarInterface.Module))
+  Drivers.compile(
+    dir,
+    () => new CompilationUnit2.Foo,
+    () => new (CompilationUnit1.BarInterface.Module)
+  )
   Drivers.link(new java.io.File(dir + "/Foo.sv"))
 
 }
