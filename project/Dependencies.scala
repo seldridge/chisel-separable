81e49d72639e6922a901c7f617ffde5b85c2870e
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2023-03-20 16:08:19 -0400

    Bump chisel 5 snapshot
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
-  private val chiselVersion = "5.0.0-M1+7-73cdddb1-SNAPSHOT"
+  private val chiselVersion = "5.0.0-M1+32-baed66d4-SNAPSHOT"
   lazy val chisel3 = "org.chipsalliance" %% "chisel" % chiselVersion
   lazy val chiselCompilerPlugin = "org.chipsalliance" %% "chisel-plugin" % chiselVersion
 }
